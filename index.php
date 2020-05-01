d785a3170f3a527721396d4cc429b701dad079a0
abulubabah <rezamalikdrive@gmail.com>
2020-05-01 16:31:34 +0700

    with fixed sql including data and csv for un_siswa data

--- index.php
+++ index.php
@@ -36,8 +36,8 @@ $timestamp = strtotime($hsl['tgl_pengumuman']);
             <div id="navbar" class="collapse navbar-collapse">
               <ul class="nav navbar-nav navbar-right">
                 <li><a href="./">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <!-- <li><a href="#about">About</a></li> -->
+                <!-- <li><a href="#contact">Contact</a></li> -->
               </ul>
             </div><!--/.nav-collapse -->
         </div>
